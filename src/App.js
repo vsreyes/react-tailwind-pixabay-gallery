12b354523aaa641334daf6e43b6dc4a23e6f26bf
vsreyes <virgilio.s.reyes@gmail.com>
2021-02-23 18:03:01 +0100

    Api setup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
   const [isLoading, setLoading] = useState(true);
   const [term, setTerm] = useState('');
 
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  });
+
   return (
     <div class='max-w-sm rounded overflow-hidden shadow-lg'>
       <img src='https://source.unsplash.com/random' alt='' className='w-full' />
